a66446dd076e3a5173b875e0165111213628021c
Veslo4123 <vadimvolodin06@gmail.com>
2022-10-18 16:50:07 +0300

    patch 0.0.4

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import telebot
 
-bot = telebot.TeleBot
+bot = telebot.TeleBot('5504467462:AAFM21r5kFEX9PmVhg8j308cvDYykknm1hU')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет, пользователь')
