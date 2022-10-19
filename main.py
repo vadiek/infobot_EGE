707bc995b60afb7f4a5f0ba4b502eaf0f9df7085
Veslo4123 <vadimvolodin06@gmail.com>
2022-10-19 20:46:26 +0300

    Patch 0.0.6

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
+from unittest.mock import call
+
 import telebot
-from telebot import types
+from keyboa.keyboards import keyboa_maker
+
+from libs.telebot import types
+from keyboa import Keyboa
 
 bot = telebot.TeleBot('5504467462:AAFM21r5kFEX9PmVhg8j308cvDYykknm1hU')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.InlineKeyboardMarkup()
 
-    f_button = types.InlineKeyboardButton('Информация о ЕГЭ', callback_data='1')
-    s_button = types.InlineKeyboardButton('Расписание ЕГЭ', callback_data='1')
-    t_button = types.InlineKeyboardButton('Минимальные баллы', callback_data='1')
-    forth_button = types.InlineKeyboardButton('Баллы для поступления', callback_data='1')
-    fifth_button = types.InlineKeyboardButton('Баллы за прошлый год', callback_data='1')
-    sixes_button = types.InlineKeyboardButton('Где можно посмотреть результаты', callback_data='1')
-    seventh_button = types.InlineKeyboardButton('Перевод первичных баллов', callback_data='1')
+    buttons = [
+        'Информация о ЕГЭ', 'Расписание ЕГЭ', 'Минимальные баллы',
+        'Баллы для поступления', 'Баллы за прошлый год', 'Где можно посмотреть результаты',
+        'Перевод первичных баллов'
+    ]
 
-    markup.add(f_button, s_button, t_button, forth_button, fifth_button, sixes_button, seventh_button)
+    markup = keyboa_maker(items=buttons, copy_text_to_callback=True, items_in_row=1)
 
     bot.send_message(message.chat.id,
                      'Привет, пользователь, данный бот создан для информирования пользователей о различных аспектах ЕГЭ \n'
                      '\nНиже представлен список доступной информации, нажимая на кнопки вы будете получать информацию нужную вам',
                      reply_markup=markup)
-
 
 bot.polling(none_stop=True)
